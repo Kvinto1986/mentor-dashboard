abddfc9bc1b91e41057aec0f5efe8582a782cf07
Kvinto1986 <kvinto1986@gmail.com>
2019-02-03 18:12:55 +0300

    Updates

---
+++ precache-manifest.338565d80acb4c6fc03b6264154041b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mentor-dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad02569271f22ba58a3",
+    "url": "/mentor-dashboard/static/js/runtime~main.5ad02569.js"
+  },
+  {
+    "revision": "e5b6446db33e9ebbb04d",
+    "url": "/mentor-dashboard/static/js/main.e5b6446d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mentor-dashboard/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e5b6446db33e9ebbb04d",
+    "url": "/mentor-dashboard/static/css/main.1476cb2a.chunk.css"
+  },
+  {
+    "revision": "6a29aaa6fc070162dbbd331b9776ac18",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
